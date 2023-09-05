67aa793ca7790be0a354ddc2c1beac84c7a40551
Tharles de Sousa Andrade <irtharles@gmail.com>
2023-09-05 19:01:07 -0300

    handle some errors.

--- pme.py
+++ pme.py
@@ -8,7 +8,7 @@ from qgis.gui import QgsMapCanvas
 from qgis.utils import iface
 from datetime import datetime
 
-PLANET_API_KEY = ''
+PLANET_API_KEY = 'e5ab79625244442295b4093991353285'
 
 layerGridDockWidgetInstance = None
 
@@ -49,8 +49,10 @@ class LayerGridDockWidget(QDockWidget):
         def sort_key(layer):
             try:
                 return datetime.strptime(layer.name(), '%m/%Y')
-            except ValueError:
-                return float('inf')
+            except Exception as e:
+                msg = iface.messageBar().createMessage("sort_key", f"Error -> {e}")
+                iface.messageBar().pushWidget(msg, level=Qgis.Critical)
+                pass
 
         sorted_layers = sorted(layers, key=sort_key, reverse=True)
 
@@ -75,12 +77,19 @@ class LayerGridDockWidget(QDockWidget):
                 row += 2
 
     def sync_zoom(self):
-        main_canvas = iface.mapCanvas()
-        extent = main_canvas.extent()
+        try:
+            main_canvas = iface.mapCanvas()
+            extent = main_canvas.extent()
 
-        for canvas in self.canvases:
-            canvas.setExtent(extent)
-            canvas.refresh()
+            for canvas in self.canvases:
+                canvas.setExtent(extent)
+                canvas.refresh()
+                
+        except Exception as e:
+            msg = iface.messageBar().createMessage("sort_key", f"Error -> {e}")
+            iface.messageBar().pushWidget(msg, level=Qgis.Critical)
+            pass
+
 
 
 class PlanetMosaicExplorerWidget(QWidget):
@@ -91,100 +100,105 @@ class PlanetMosaicExplorerWidget(QWidget):
             msg = iface.messageBar().createMessage("FETCH_MOSAICS", "Please provide your PLANET_API_KEY.")
             iface.messageBar().pushWidget(msg, level=Qgis.Critical)
             return
+        try:
+            self.layout = QVBoxLayout()
+            self.label = QLabel("Month: ...")
+            self.label.setStyleSheet("color: green; font-size: 25px;")
+            self.label.setAlignment(Qt.AlignCenter)
+            self.layout.addWidget(self.label)
 
-        self.layout = QVBoxLayout()
-        self.label = QLabel("Month: ...")
-        self.label.setStyleSheet("color: green; font-size: 25px;")
-        self.label.setAlignment(Qt.AlignCenter)
-        self.layout.addWidget(self.label)
+            # Create a horizontal layout for the slider and buttons
+            self.hbox = QHBoxLayout()
 
-        # Create a horizontal layout for the slider and buttons
-        self.hbox = QHBoxLayout()
+            self.startLabel = QLabel("Start Date:")
+            self.endLabel = QLabel("End Date:")
 
-        self.startLabel = QLabel("Start Date:")
-        self.endLabel = QLabel("End Date:")
+            # Add date filters
+            self.hbox.addWidget(self.startLabel)
+            self.startDateEdit = QDateEdit()
+            self.startDateEdit.setDate(QDate.currentDate())
+            self.startDateEdit.setCalendarPopup(True)
+            self.startDateEdit.setFixedWidth(100)
+            self.startDateEdit.setDisplayFormat("dd/MM/yyyy")
+            self.hbox.addWidget(self.startDateEdit)
 
-        # Add date filters
-        self.hbox.addWidget(self.startLabel)
-        self.startDateEdit = QDateEdit()
-        self.startDateEdit.setDate(QDate.currentDate())
-        self.startDateEdit.setCalendarPopup(True)
-        self.startDateEdit.setFixedWidth(100)
-        self.startDateEdit.setDisplayFormat("dd/MM/yyyy")
-        self.hbox.addWidget(self.startDateEdit)
+            self.hbox.addWidget(self.endLabel)
+            self.endDateEdit = QDateEdit()
+            self.endDateEdit.setDate(QDate.currentDate())
+            self.endDateEdit.setCalendarPopup(True)
+            self.endDateEdit.setFixedWidth(100)
+            self.endDateEdit.setDisplayFormat("dd/MM/yyyy")
+            self.hbox.addWidget(self.endDateEdit)
 
-        self.hbox.addWidget(self.endLabel)
-        self.endDateEdit = QDateEdit()
-        self.endDateEdit.setDate(QDate.currentDate())
-        self.endDateEdit.setCalendarPopup(True)
-        self.endDateEdit.setFixedWidth(100)
-        self.endDateEdit.setDisplayFormat("dd/MM/yyyy")
-        self.hbox.addWidget(self.endDateEdit)
+            self.coordInput = QLineEdit(self)
+            self.coordInput.setPlaceholderText("Point lat,lon (EPSG:4326)")
+            self.hbox.addWidget(self.coordInput)
 
-        self.coordInput = QLineEdit(self)
-        self.coordInput.setPlaceholderText("Point lat,lon (EPSG:4326)")
-        self.hbox.addWidget(self.coordInput)
+            self.slider = QSlider(Qt.Horizontal)
+            self.slider.setTickInterval(1)
+            self.slider.setValue(1)
 
-        self.slider = QSlider(Qt.Horizontal)
-        self.slider.setTickInterval(1)
-        self.slider.setValue(1)
+            self.timer = QTimer(self)
+            self.timer.timeout.connect(self.play_timelapse)
 
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.play_timelapse)
+            self.filterButton = QPushButton("Filter")
+            self.filterButton.setCursor(Qt.PointingHandCursor)
+            self.filterButton.setIcon(QgsApplication.getThemeIcon('/mActionFilter.svg'))
+            self.filterButton.setToolTip('Apply Date Filter')
+            self.hbox.addWidget(self.filterButton)
 
-        self.filterButton = QPushButton("Filter")
-        self.filterButton.setCursor(Qt.PointingHandCursor)
-        self.filterButton.setIcon(QgsApplication.getThemeIcon('/mActionFilter.svg'))
-        self.filterButton.setToolTip('Apply Date Filter')
-        self.hbox.addWidget(self.filterButton)
+            self.clearFilterButton = QPushButton("Clear")
+            self.clearFilterButton.setIcon(QgsApplication.getThemeIcon("/mIconClearText.svg"))
+            self.clearFilterButton.setCursor(Qt.PointingHandCursor)
+            self.filterButton.setToolTip('Clear Date Filter')
+            self.hbox.addWidget(self.clearFilterButton)
 
-        self.clearFilterButton = QPushButton("Clear")
-        self.clearFilterButton.setIcon(QgsApplication.getThemeIcon("/mIconClearText.svg"))
-        self.clearFilterButton.setCursor(Qt.PointingHandCursor)
-        self.filterButton.setToolTip('Clear Date Filter')
-        self.hbox.addWidget(self.clearFilterButton)
+            self.playButton = QPushButton("Play")
+            self.playButton.setIcon(QgsApplication.getThemeIcon("/mActionPlay.svg"))
+            self.playButton.setCursor(Qt.PointingHandCursor)
+            self.hbox.addWidget(self.playButton)
 
-        self.playButton = QPushButton("Play")
-        self.playButton.setIcon(QgsApplication.getThemeIcon("/mActionPlay.svg"))
-        self.playButton.setCursor(Qt.PointingHandCursor)
-        self.hbox.addWidget(self.playButton)
+            self.stopButton = QPushButton("Stop")
+            self.stopButton.setIcon(QgsApplication.getThemeIcon("/mActionStop.svg"))
+            self.stopButton.setCursor(Qt.PointingHandCursor)
+            self.hbox.addWidget(self.stopButton)
 
-        self.stopButton = QPushButton("Stop")
-        self.stopButton.setIcon(QgsApplication.getThemeIcon("/mActionStop.svg"))
-        self.stopButton.setCursor(Qt.PointingHandCursor)
-        self.hbox.addWidget(self.stopButton)
+            self.gridButton = QPushButton("Grid")
+            self.gridButton.setIcon(QgsApplication.getThemeIcon("/mActionNewTable.svg"))
+            self.gridButton.setCursor(Qt.PointingHandCursor)
+            self.hbox.addWidget(self.gridButton)
 
-        self.gridButton = QPushButton("Grid")
-        self.gridButton.setIcon(QgsApplication.getThemeIcon("/mActionNewTable.svg"))
-        self.gridButton.setCursor(Qt.PointingHandCursor)
-        self.hbox.addWidget(self.gridButton)
+            self.removeButton = QPushButton("Remove Layers")
+            self.removeButton.setIcon(QgsApplication.getThemeIcon("/mActionFileExit.svg"))
+            self.removeButton.setCursor(Qt.PointingHandCursor)
+            self.hbox.addWidget(self.removeButton)
 
-        self.removeButton = QPushButton("Remove Layers")
-        self.removeButton.setIcon(QgsApplication.getThemeIcon("/mActionFileExit.svg"))
-        self.removeButton.setCursor(Qt.PointingHandCursor)
-        self.hbox.addWidget(self.removeButton)
+            # Add the horizontal layout to the vertical layout
+            self.layout.addLayout(self.hbox)
+            self.layout.addWidget(self.slider)
+            # Set the layout for the QWidget
+            self.setLayout(self.layout)
 
-        # Add the horizontal layout to the vertical layout
-        self.layout.addLayout(self.hbox)
-        self.layout.addWidget(self.slider)
-        # Set the layout for the QWidget
-        self.setLayout(self.layout)
+            # Connect events
+            self.removeButton.clicked.connect(self.remove_layers)
+            self.playButton.clicked.connect(self.start_timelapse)
+            self.stopButton.clicked.connect(self.stop_timelapse)
+            self.slider.valueChanged.connect(self.slider_changed)
+            self.filterButton.clicked.connect(self.filter_layers)
+            self.clearFilterButton.clicked.connect(self.clear_filter)
+            self.gridButton.clicked.connect(self.change_visibility_grid)
 
-        # Connect events
-        self.removeButton.clicked.connect(self.remove_layers)
-        self.playButton.clicked.connect(self.start_timelapse)
-        self.stopButton.clicked.connect(self.stop_timelapse)
-        self.slider.valueChanged.connect(self.slider_changed)
-        self.filterButton.clicked.connect(self.filter_layers)
-        self.clearFilterButton.clicked.connect(self.clear_filter)
-        self.gridButton.clicked.connect(self.change_visibility_grid)
+            self.setLayout(self.layout)
+            self.layer_ids = []
+            self.current_layer_id = None
+            self.current_layer_name = None
+            self.mosaics_data = []
+            self.fetch_mosaics()
+        except Exception as e:
+            msg = iface.messageBar().createMessage("sort_key", f"Error -> {e}")
+            iface.messageBar().pushWidget(msg, level=Qgis.Critical)
+            pass
 
-        self.setLayout(self.layout)
-        self.layer_ids = []
-        self.current_layer_id = None
-        self.current_layer_name = None
-        self.mosaics_data = []
-        self.fetch_mosaics()
 
     def fetch_mosaics(self):
         try:
@@ -214,28 +228,33 @@ class PlanetMosaicExplorerWidget(QWidget):
             print(f"Error fetching mosaics: {e}")
 
     def init(self):
-        # Sort mosaics by date in descending order (most recent first)
-        self.mosaics_data.sort(key=lambda x: datetime.strptime(x['name'], '%m/%Y'), reverse=True)
+        try:
+            # Sort mosaics by date in descending order (most recent first)
+            self.mosaics_data.sort(key=lambda x: datetime.strptime(x['name'], '%m/%Y'), reverse=True)
 
-        self.slider.setMinimum(1)
-        self.slider.setMaximum(len(self.mosaics_data))
+            self.slider.setMinimum(1)
+            self.slider.setMaximum(len(self.mosaics_data))
 
-        # Initialize layers (assuming they are already added)
-        self.layer_ids = []
+            # Initialize layers (assuming they are already added)
+            self.layer_ids = []
 
-        for mosaic in self.mosaics_data:
-            layer_id = self.add_layer(mosaic)
-            if layer_id:
-                self.layer_ids.append(layer_id)
+            for mosaic in self.mosaics_data:
+                layer_id = self.add_layer(mosaic)
+                if layer_id:
+                    self.layer_ids.append(layer_id)
 
-        # Initially, make the first layer visible
-        self.current_layer_id = self.layer_ids[0]
-        self.label.setText(f"Month: {self.mosaics_data[0]['name']}")
-        self.label.setStyleSheet("color: green; font-size: 25px;")
-        QgsProject.instance().layerTreeRoot().findLayer(
-            self.current_layer_id
-        ).setItemVisibilityChecked(True)
-
+            # Initially, make the first layer visible
+            self.current_layer_id = self.layer_ids[0]
+            self.label.setText(f"Month: {self.mosaics_data[0]['name']}")
+            self.label.setStyleSheet("color: green; font-size: 25px;")
+            QgsProject.instance().layerTreeRoot().findLayer(
+                self.current_layer_id
+            ).setItemVisibilityChecked(True)
+        except Exception as e:
+            msg = iface.messageBar().createMessage("init", f"Error -> {e}")
+            iface.messageBar().pushWidget(msg, level=Qgis.Critical)
+            pass
+            
     def add_layer(self, mosaic):
         tile_url = mosaic['_links']['tiles']
         service_url = tile_url.replace('=', '%3D').replace('&', '%26')
@@ -252,63 +271,73 @@ class PlanetMosaicExplorerWidget(QWidget):
             return layer.id()
 
         return None
-
+        
     def zoom_to_point(self):
-        coord_text = self.coordInput.text()
-        lat, lon = map(float, coord_text.split(','))
+        try:
+            coord_text = self.coordInput.text()
+            lat, lon = map(float, coord_text.split(','))
 
-        # Convert from EPSG:4326 to EPSG:3857
-        source_crs = QgsCoordinateReferenceSystem(4326)
-        dest_crs = QgsCoordinateReferenceSystem(3857)
-        transform = QgsCoordinateTransform(source_crs, dest_crs, QgsProject.instance())  # Include project instance
+            # Convert from EPSG:4326 to EPSG:3857
+            source_crs = QgsCoordinateReferenceSystem(4326)
+            dest_crs = QgsCoordinateReferenceSystem(3857)
+            transform = QgsCoordinateTransform(source_crs, dest_crs, QgsProject.instance())  # Include project instance
 
-        point_4326 = QgsPointXY(lon, lat)
-        point_3857 = transform.transform(point_4326)
+            point_4326 = QgsPointXY(lon, lat)
+            point_3857 = transform.transform(point_4326)
 
-        # Assuming iface.mapCanvas() returns the map canvas
-        canvas = iface.mapCanvas()
-        canvas.setCenter(point_3857)
-        canvas.zoomScale(20000)  # Zoom level, you can adjust this
-        canvas.refresh()
+            # Assuming iface.mapCanvas() returns the map canvas
+            canvas = iface.mapCanvas()
+            canvas.setCenter(point_3857)
+            canvas.zoomScale(20000)  # Zoom level, you can adjust this
+            canvas.refresh()
+        except Exception as e:
+            msg = iface.messageBar().createMessage("filter_layers", f"Error -> {e}")
+            iface.messageBar().pushWidget(msg, level=Qgis.Critical)
+            pass
 
     def filter_layers(self):
-        # Fetch selected start and end dates
-        start_date = self.startDateEdit.date().toString("MM/yyyy")
-        end_date = self.endDateEdit.date().toString("MM/yyyy")
+        try:
+            # Fetch selected start and end dates
+            start_date = self.startDateEdit.date().toString("MM/yyyy")
+            end_date = self.endDateEdit.date().toString("MM/yyyy")
 
-        # Convert to Python datetime.date for easier comparison
-        start_date_py = datetime.strptime(start_date, '%m/%Y').date()
-        end_date_py = datetime.strptime(end_date, '%m/%Y').date()
+            # Convert to Python datetime.date for easier comparison
+            start_date_py = datetime.strptime(start_date, '%m/%Y').date()
+            end_date_py = datetime.strptime(end_date, '%m/%Y').date()
 
-        # Filter self.mosaics_data based on date, updating in-place
-        temp = self.mosaics_data
-        self.mosaics_data = [
-            mosaic for mosaic in self.mosaics_data
-            if start_date_py <= datetime.strptime(mosaic['name'], '%m/%Y').date() <= end_date_py
-        ]
+            # Filter self.mosaics_data based on date, updating in-place
+            temp = self.mosaics_data
+            self.mosaics_data = [
+                mosaic for mosaic in self.mosaics_data
+                if start_date_py <= datetime.strptime(mosaic['name'], '%m/%Y').date() <= end_date_py
+            ]
 
-        if len(self.mosaics_data) == 0:
-            msg = iface.messageBar().createMessage("FILTER", "No result found for the selected date range.")
-            button = QAction("Got it", iface.messageBar())
-            button.triggered.connect(lambda: iface.messageBar().popWidget(msg))
-            msg.addAction(button)
-            iface.messageBar().pushWidget(msg, level=Qgis.Critical)
-            self.mosaics_data = temp
+            if len(self.mosaics_data) == 0:
+                msg = iface.messageBar().createMessage("FILTER", "No result found for the selected date range.")
+                button = QAction("Got it", iface.messageBar())
+                button.triggered.connect(lambda: iface.messageBar().popWidget(msg))
+                msg.addAction(button)
+                iface.messageBar().pushWidget(msg, level=Qgis.Critical)
+                self.mosaics_data = temp
+                del temp
+                return
+
+            # Update the slider's maximum value based on the filtered list
+            self.slider.setMaximum(len(self.mosaics_data) - 1)
+
+            # Remove existing layers and re-initialize based on filtered data
+            self.remove_layers()
+            self.init()
+
+            coord = self.coordInput.text()
+            if coord:
+                self.zoom_to_point()
+
             del temp
-            return
-
-        # Update the slider's maximum value based on the filtered list
-        self.slider.setMaximum(len(self.mosaics_data) - 1)
-
-        # Remove existing layers and re-initialize based on filtered data
-        self.remove_layers()
-        self.init()
-
-        coord = self.coordInput.text()
-        if coord:
-            self.zoom_to_point()
-
-        del temp
+        except Exception as e:
+            msg = iface.messageBar().createMessage("filter_layers", f"Error -> {e}")
+            iface.messageBar().pushWidget(msg, level=Qgis.Critical)
+            pass
 
     def clear_filter(self):
         # Resetting the date filters to their initial state
@@ -339,20 +368,25 @@ class PlanetMosaicExplorerWidget(QWidget):
             self.slider.setValue(current_value + 1)
 
     def slider_changed(self):
-        index = self.slider.value() - 1
+        try:
+            index = self.slider.value() - 1
 
-        # Hide the previous layer
-        if self.current_layer_id:
-            QgsProject.instance().layerTreeRoot().findLayer(
-                self.current_layer_id
-            ).setItemVisibilityChecked(False)
+            # Hide the previous layer
+            if self.current_layer_id:
+                QgsProject.instance().layerTreeRoot().findLayer(
+                    self.current_layer_id
+                ).setItemVisibilityChecked(False)
 
-        # Show the current layer
-        self.current_layer_id = self.layer_ids[index]
-        layer = QgsProject.instance().layerTreeRoot().findLayer(self.current_layer_id)
-        layer.setItemVisibilityChecked(True)
-        self.label.setText(f"Month: {layer.name()}")
-        self.label.setStyleSheet("color: green; font-size: 25px;")
+            # Show the current layer
+            self.current_layer_id = self.layer_ids[index]
+            layer = QgsProject.instance().layerTreeRoot().findLayer(self.current_layer_id)
+            layer.setItemVisibilityChecked(True)
+            self.label.setText(f"Month: {layer.name()}")
+            self.label.setStyleSheet("color: green; font-size: 25px;")
+        except Exception as e:
+            msg = iface.messageBar().createMessage("filter_layers", f"Error -> {e}")
+            iface.messageBar().pushWidget(msg, level=Qgis.Critical)
+            pass
 
     def change_visibility_grid(self):
         global layerGridDockWidgetInstance
